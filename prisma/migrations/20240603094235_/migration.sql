c2d34e1df9fd415291994a06b681f8a1ec4e5ede
Marcel Maier (Ubuntu) <marcelmaier1996@outlook.de>
2024-06-03 12:51:52 +0200

    added seeding script to create (n) products with (m) random categories

---
+++ prisma/migrations/20240603094235_/migration.sql
@@ -0,0 +1,42 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `category` on the `Product` table. All the data in the column will be lost.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "category";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductCategory" (
+    "productId" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductCategory_pkey" PRIMARY KEY ("productId","categoryId")
+);
+
+-- AddForeignKey
+ALTER TABLE "ProductCategory" ADD CONSTRAINT "ProductCategory_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductCategory" ADD CONSTRAINT "ProductCategory_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,10 +1,15 @@
 /**
- * ! Executing this script will delete all data in your database and seed it with 10 users.
+ * ! Executing this script will delete all data in your database and seed it with products and categories.
  * ! Make sure to adjust the script to your needs.
- * Use any TypeScript runner to run this script, for example: `npx tsx seed.ts`
+ * Use any TypeScript runner to run this script, for example: `npx ts-node seed.ts`
  * Learn more about the Seed Client by following our guide: https://docs.snaplet.dev/seed/getting-started
  */
 import { createSeedClient } from "@snaplet/seed";
+
+const productsCount = 10;
+const minPrice = 10;
+const maxPrice = 1000;
+const categoriesCount = 10;
 
 async function main() {
     const seed = await createSeedClient();
@@ -12,18 +17,17 @@ async function main() {
     // Truncate all tables in the database
     await seed.$resetDatabase();
 
-    // Seed the database with 10 users
-    await seed.user((createMany) => createMany(10, {
-        // Create 10 posts for each of those users
-        posts: (createMany) => createMany(10),
-    }))
 
-    // Seed the database with 10 products
-    await seed.product((createMany) => createMany(10))
+    await seed.product((x) => x(productsCount, (ctx) => ({
+        name: `Product #${ctx.index}`,
+        description: `Product description #${ctx.index}`,
+        price: Math.floor(Math.random() * (maxPrice - minPrice) + minPrice),
+        image: `https://picsum.photos/200/300?random=${ctx.index}`,
+        categories: (x) => x({ min: 1, max: 5 })
+    })))
 
-    console.log("Database seeded successfully!");
 
     process.exit();
-};
+}
 
 main();
